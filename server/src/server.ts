2515db9c65d1a762241a8591d4809df42a4f6054
Mitacho <arthurf@tuta.io>
2022-05-24 07:46:18 -0300

    chore(stage 3): 42:10

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.post("/feedbacks", (req, res) => {
+  console.log(req.body);
+
+  res.status(201).send();
+});
+
 app.listen(3333, () => {
   console.log("Server started on port 3333!");
 });
