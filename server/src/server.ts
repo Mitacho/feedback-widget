4cc13100c1c6f029a623fca5edbb9a1b45ce98ce
Mitacho <arthurf@tuta.io>
2022-05-25 07:33:33 -0300

    chore(stage 3): 54:40

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,44 @@
 import express from "express";
+import nodemailer from "nodemailer";
+import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/feedbacks", (req, res) => {
-  console.log(req.body);
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "1638c55ca96634",
+    pass: "d6fe40aab2a875",
+  },
+});
 
-  res.status(201).send();
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Arthur Flávio <arthurflavionz@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
+  res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
