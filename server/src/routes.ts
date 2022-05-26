8fe20349f4432f9bdba1ea8488984c9acd0272f7
Mitacho <arthurf@tuta.io>
2022-05-26 07:30:25 -0300

    refactor: create a routes file

---
+++ server/src/routes.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import { prisma } from "./prisma";
+
+export const routes = express.Router();
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "1638c55ca96634",
+    pass: "d6fe40aab2a875",
+  },
+});
+
+routes.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Arthur Flávio <arthurflavionz@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
+  res.status(201).json({ data: feedback });
+});
