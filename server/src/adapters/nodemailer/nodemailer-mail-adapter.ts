30985f45d487a730598945486a8244f23228b267
Mitacho <arthurf@tuta.io>
2022-05-27 19:48:59 -0300

    chore(stage 3): apply SOLID principles

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "1638c55ca96634",
+    pass: "d6fe40aab2a875",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Arthur Flávio <arthurflavionz@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,40 +1,25 @@
 import express from "express";
-import nodemailer from "nodemailer";
-import { prisma } from "./prisma";
+import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "1638c55ca96634",
-    pass: "d6fe40aab2a875",
-  },
-});
 
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const nodemailerMailAdapter = new NodemailerMailAdapter();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
+  );
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
   });
 
-  await transport.sendMail({
-    from: "Equipe Feedget <oi@feedget.com>",
-    to: "Arthur Flávio <arthurflavionz@gmail.com>",
-    subject: "Novo feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`,
-    ].join("\n"),
-  });
-
-  res.status(201).json({ data: feedback });
+  res.status(201).send();
 });
